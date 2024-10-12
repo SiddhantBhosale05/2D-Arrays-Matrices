d93d9ccb0d1baf1a271844af5e75a2ca2fc442d5
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:18:04 +0530

    Transpose of Matrices.cpp

--- Transpose of Matrices.cpp
+++ Transpose of Matrices.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of 2D Arrays - Matrices to Transpose of Matrices
+//Aim:Implementation of 2D Arrays - Matrices to Transpose of Matrices
 
 #include <iostream>
 using namespace std;
