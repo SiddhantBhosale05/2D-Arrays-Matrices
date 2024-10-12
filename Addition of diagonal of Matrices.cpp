50d06a5082878542c09e65d976afdc179c37d2c9
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:17:08 +0530

    Addition of diagonal of Matrices.cpp

--- Addition of diagonal of Matrices.cpp
+++ Addition of diagonal of Matrices.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of 2D Arrays - Matrices to Add diagonal of Matrices
+//Aim:Implementation of 2D Arrays - Matrices to Add diagonal of Matrices
 
 #include <iostream>
 using namespace std;
